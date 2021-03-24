1c03f4adb2c992746999e408f431b27354571702
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-24 14:14:04 +0100

    Aggiunto esercizio con bonus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
-  // Inserisci il codice qui
+  var app = new Vue(
+    {
+      el: '#container',
+      data: {
+        message: 'CIAO! Questa è la foto che ho scelto per te! Spero ti piaccia!',
+        image: 'foto-1.jpg'
+      }
+    }
+  );
 
 });
